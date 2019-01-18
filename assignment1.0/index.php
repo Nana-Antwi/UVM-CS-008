c3d6b6fefe4938c6178f42ad75cbc5c437bf21d7
Nana-Antwi <46608200+Nana-Antwi@users.noreply.github.com>
2019-01-18 13:50:17 -0500

    Add files via upload

---
+++ assignment1.0/index.php
@@ -0,0 +1,30 @@
+<?php
+/* php code can go anywhere but we will be starting almost all our documents
+ * with php code There is no code here at the moment */
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>Making The World A Better Place</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Nana Antwi">
+        <meta name="description" content="Making The World A Better Place">
+    </head>
+
+    <body>
+        <h1>Nana Antwi</h1>
+
+        <h2>Making The world A Better Place</h2>
+
+        <h3><a href="tryme.php">Assignment1.0</a></h3>
+
+        <h4><a href="specdcu.pdf">Specification Document</a></h4>
+
+        <img src="pic.jpg"
+    </body>
+
+
+
+
+</html>
---
+++ assignment1.0/tryme.php
@@ -0,0 +1,23 @@
+<?php
+/* php code can go anywhere but we will be starting almost all our documents
+ * with php code There is no code here at the moment */
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>Making The World A Better Place</title>
+    </head>
+
+    <body>
+
+        <h1>Assignment1.0</h1>
+
+        <p>Making The World A Better Place</p>
+
+        <p>The world for centuries has had its fair share of problems. Thus the need to make it a better a place. 
+            Issues like; Global Warming, Hunger, Political Unrest, Natural Disaster and Economic downturns cause an imbalance in our societies.
+            The creation of programs like; the world program, disaster relief, peace keeping campaigns and government bail outs etc, has brought a little close in achieving our millennium development goals.
+            As humans we have to be proactive and join all efforts of making the world a better place because earth is life and life is us.</p>
+    </body>
+</html>
