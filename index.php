656f290af008773cd4a5e830a4d791b261382620
Nana-Antwi <46608200+Nana-Antwi@users.noreply.github.com>
2019-01-18 14:03:24 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/* php code can go anywhere but we will be starting almost all our documents
+ * with php code There is no code here at the moment */
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>Making The World A Better Place</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Nana Antwi">
+        <meta name="description" content="Making The World A Better Place">
+    </head>
+
+    <body>
+
+        <h1>Nana Antwi</h1>
+
+        <h2>Making The world A Better Place</h2>
+        
+        <ol>
+
+            <li><a href="assignment1.0/">Assignment 1.0</a></li>
+            <li><a href="assignment1.1/">Assignment 1.1</a></li>
+            <li><a href="assignment1.2/">Assignment 1.2</a></li>
+            <li><a href="assignment1.3/">Assignment 1.3</a></li>
+            <li><a href="assignment1.4/">Assignment 1.4</a></li>
+            <li><a href="assignment1.5/">Assignment 1.5</a></li>
+            <li><a href="assignment1.6/">Assignment 1.6</a></li>
+            <li><a href="assignment1.7/">Assignment 1.7</a></li>
+            <li><a href="assignment8.0/">Assignment 8.0</a></li>
+            <li><a href="assignment9.0/">Assignment 9.0</a></li>
+        
+        </ol>
+
+        <img src="betterplace.jpg">
+
+
+    </body>
+
+</html>
